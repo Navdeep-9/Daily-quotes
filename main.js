97d8047d1bf51168d6cbbb27b2826c81230370c2
Navdeep-9 <158136580+Navdeep-9@users.noreply.github.com>
2025-07-01 23:47:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const quote = document.getElementById("quote")
 const author = document.getElementById("author")
 
 
-const api_url = "https://api.quotable.io/random";
+const api_url = "https://quotes-db.vercel.app/api/random";
 
 
 async function getquote(url){
     const response = await fetch(url);
     var data = await response.json();
     // console.log(data);
-    quote.innerHTML = data.content
+    quote.innerHTML = data.quote
     author.innerHTML = data.author
 }
 
